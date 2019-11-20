fa6f7b092b63e9476b05f62882f9af3a7c45359e
Mehdkit <55589811+Mehdkit@users.noreply.github.com>
2019-11-20 15:50:23 +0100

    mouvement servo codé
    Vérifier si les transition entre phase sont correcte
    Ajouter la commande du moteur

--- code/debut_code_robot_s5/debut_code_robot_s5.ino
+++ code/debut_code_robot_s5/debut_code_robot_s5.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 
 #define sensor A0 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
 
@@ -7,8 +9,13 @@
 #define PHASE_ESCALIER 3
 #define PHASE_PALIER 4
 #define PHASE_FINAL 5
+#define PIN_SERVO_1 3
+#define PIN_SERVO_2 4
 
-#define TROP_LOIN 424242424242
+#define ANGLE_HAUT 70
+#define ANGLE_BAS -70
+
+#define TROP_LOIN 42424242
 
 /* ==== CONSTANTES CAPTEUR US ====== */
 
@@ -22,6 +29,12 @@ const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = ~8m à 340m/s
 /* Vitesse du son dans l'air en mm/us */
 const float SOUND_SPEED = 340.0 / 1000;
 
+const float var_angle_par_tick = 0.1;
+
+Servo Servo1;
+Servo Servo2;
+
+int passe = 0;
 int etat = ETAT_INIT;
 
 int bouton_activation = 0;
@@ -34,6 +47,10 @@ void setup() {
   pinMode(TRIGGER_PIN, OUTPUT);
   digitalWrite(TRIGGER_PIN, LOW); // La broche TRIGGER doit être à LOW au repos
   pinMode(ECHO_PIN, INPUT);
+  
+  /*Initialisation des servo*/
+  Servo1.attach(PIN_SERVO_1);
+  Servo2.attach(PIN_SERVO_2);
 }
 
 void loop() {
@@ -49,6 +66,7 @@ void loop() {
   //Selon la phase dans laquele on est, on effectue cette loop
   switch(etat){
     case ETAT_INIT:
+      //inclure la lecture des button et du bluetooth
       //On attend que le bouton passe à l'etat 1
       if(bouton_activation == 1){
         etat = PHASE_PLAT;
@@ -56,8 +74,6 @@ void loop() {
       break;
 
     case PHASE_PLAT:
-      //On avance tout droit tant que l'on est pas à moins de 8cm de la première marche
-      
       //Lorsque l'on est à 8 cm de la marche, on passe en etat 
       if(get_distance_US() <= 80){
         etat = PHASE_AVANT_ESC;
@@ -65,29 +81,37 @@ void loop() {
       break;
 
     case PHASE_AVANT_ESC:
-      //On continue d'avancer
-      
       //On se place dans l'angle d'approche
-      
+      Servo1.write(ANGLE_HAUT);
+      Servo2.write(ANGLE_HAUT);
       //Condition de passage à la phase suivante : le capteur IR passe en dessous de 3.5 cm
       if(get_distance_IR() <= 35){
         etat = PHASE_ESCALIER;
       }
       break;
     case PHASE_ESCALIER:
+      
       //on se remet à plat lentement et on avance jusqu'au palier
-
-      //Condition de passage à la phase suivante : le capteur IR passe au dessus de 15 cm
-      if(get_distance_IR() >= 150){
+      Servo1.write(max(ANGLE_HAUT - passe*angle_enleve_par_tick,0));
+      Servo2.write(max(ANGLE_HAUT - passe*angle_enleve_par_tick,0));
+      //on compte le nb de passe que l'on a dans cette état
+      passe = passe + 1;
+      //Condition de passage à la phase suivante : le capteur US passe au dessus de 15 cm ? 10 ?
+      if(get_distance_US() >= 150){
+        passe = 0;
         etat = PHASE_PALIER;
       }
       break;
 
     case PHASE_PALIER:
-      //On se place dans l'angle d'arrivé, puis on se remet à plat lentement
-
-      //Condition de passage à la phase suivante : le capteur IR passe en etat NULL
-      if(get_distance_IR() == TROP_LOIN){
+      //on se remet à plat lentement et on avance jusqu'au palier
+      Servo1.write(min(ANGLE_BAS + passe*var_angle_par_tick,0));
+      Servo2.write(min(ANGLE_BAS + passe*var_angle_par_tick,0));
+      //on compte le nb de passe que l'on a dans cette état
+      passe = passe + 1;
+      
+      //Condition de passage à la phase suivante : le capteur US mesure une distance supérieure à 30cm
+      if(get_distance_US() > 300){
         etat = PHASE_FINAL;
       }
       break;
@@ -96,9 +120,14 @@ void loop() {
       //On arrete les moteur et on attend
       break;
   }
+  
+  if(etat > ETAT_INIT && etat<PHASE_FINAL){
+    //Tant que l'on est entre la phase initiale et la phase finale, on avance à la vitesse Vitesse
+    
+  }
 }
 
-float get_distance_US(){ //est ce qu'on essaye de retourné une valeur lissée sur 10 lecture ?
+float get_distance_US(){ //On retourne une valeur lissée sur 10 lectures
   float valeur_moyene = 0;
   for(int i=0;i<10;i++){
     /* 1. Lance une mesure de distance en envoyant une impulsion HIGH de 10µs sur la broche TRIGGER */
@@ -116,13 +145,17 @@ float get_distance_US(){ //est ce qu'on essaye de retourné une valeur lissée s
 }
 
 float get_distance_IR(){
-  //filtrage des valeurs 
-  float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
-  float distance = 130*pow(volts, -1); // worked out from datasheet graph
-  
-  if (distance <= 300 && distance >= 80){
-    return distance;
-  }else{
-    return TROP_LOIN;
+  float val = 0;
+  //moyennage des valeurs pour attenuer les dépassements ocasionels
+  for(int i = 0; i<10;i++){
+    float volts = analogRead(sensor)*0.0048828125;  // value from sensor * (5/1024)
+    float distance = 130*pow(volts, -1); // worked out from datasheet graph
+    val = val + distance;
   }
+  
+  if (distance <= 300 && distance >= 40){
+      return val;
+    }else{
+      return TROP_LOIN;
+    }
 }
